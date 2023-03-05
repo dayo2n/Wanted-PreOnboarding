8c3c60f6d4b27fc97a3e1185587b279a46703531
dayo2n <0217dayun@naver.com>
2023-03-05 17:16:45 +0900

    [FEAT] 프로젝트 초기 셋팅

---
+++ ImageDownloader/ImageDownloader/AppDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.swift
+//  ImageDownloader
+//
+//  Created by 문다 on 2023/03/05.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        if #available(iOS 13.0, *) {
+            return true
+        }
+        
+        window = UIWindow()
+        window?.rootViewController = ViewController() // 자신의 시작 ViewController
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ ImageDownloader/ImageDownloader/ImageDownloadCell.swift
@@ -0,0 +1,8 @@
+//
+//  ImageDownloadCell.swift
+//  ImageDownloader
+//
+//  Created by 문다 on 2023/03/05.
+//
+
+import Foundation
